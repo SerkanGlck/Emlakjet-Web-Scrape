5cd0f9ad66f64dda5f9b19988802d871f5a9920d
SerkanGlck <serkangolcuk1@gmail.com>
2024-07-18 22:59:01 +0300

    test
    test

--- Emlakjet-Web-Scrape/Emlakjet-Web-Scrape.py
+++ Emlakjet-Web-Scrape/Emlakjet-Web-Scrape.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
+# Ana sayfadaki ilanların linklerini toplamak için URL
 url = 'https://www.emlakjet.com/gunluk-kiralik-konut/'
 response = requests.get(url)
 html_data = response.text
@@ -8,39 +9,37 @@ html_data = response.text
 soup = BeautifulSoup(html_data, 'html.parser')
 
 ilanlar = soup.find_all('div', class_='_3qUI9q')
-ozellikler = soup.find('div', class_='_2UELHn')   
 
-if ilanlar or ozellikler:
- 
- for ilan in ilanlar:
+ilan_linkleri = []
 
-            ilan_basligi = ilan.find('h3')
-            ilan_fiyati = ilan.find('p', class_='_2C5UCT')
-            ilan_konumu = ilan.find('div', class_='_2wVG12')
+for ilan in ilanlar:
+    link_tag = ilan.find('a', href=True)
+    if link_tag:
+        ilan_linkleri.append(link_tag['href'])
 
-            ozellik_1 = ozellikler.find('span', {'data-test-selector': 'listing-item-property-category-1'})
-            ozellik_2 = ozellikler.find('span', {'data-test-selector': 'listing-item-property-room-count-1'})
-            ozellik_3 = ozellikler.find('span', {'data-test-selector': 'listing-item-property-floor-number-1'})
-            ozellik_4 = ozellikler.find('span', {'data-test-selector': 'listing-item-property-gross-square-1'})
-            ozellik_5 = ozellikler.find('span', {'data-test-selector': 'listing-item-property-updated-at-1'})
+for link in ilan_linkleri:
+    ilan_url = f'https://www.emlakjet.com{link}'
+    ilan_response = requests.get(ilan_url)
+    ilan_html_data = ilan_response.text
+    ilan_soup = BeautifulSoup(ilan_html_data, 'html.parser')
 
-            print(f"Ilan Başlığı: {ilan_basligi.text.strip()}")
-     
-            print(f"Fiyat: {ilan_fiyati.text.strip()}")
-                  
-            print(f"Konum: {ilan_konumu.text.strip()}")
-                   
-            print(f"Ev Tipi: {ozellik_1.text.strip()}")
-                   
-            print(f"Oda Sayisi: {ozellik_2.text.strip()}")
-                    
-            print(f"Bulundugu Kat: {ozellik_3.text.strip()}")
-                   
-            print(f"Buyukluk: {ozellik_4.text.strip()}")
-                    
-            print(f"Tarih: {ozellik_5.text.strip()}")
+    ilan_basligi = ilan_soup.find('h1', class_="_3OKyci").text.strip()
 
-            print('------------------------------')                
-           
-else:
-        print("İlanlar bulunamadı.")
+    ilan_fiyati = ilan_soup.find('div', class_='_2TxNQv').text.strip()
+
+    ilan_konumu = ilan_soup.find('div', class_='_3VQ1JB').find('p').text.strip()
+
+    print(f"İlan URL: {ilan_url}")
+    print(f"İlan Başlığı: {ilan_basligi}")
+    print(f"Fiyat: {ilan_fiyati}")
+    print(f"Konum: {ilan_konumu}")
+  
+    ilan_detaylari = ilan_soup.find_all('div', class_='_35T4WV')    
+    for detay in ilan_detaylari:
+        bilgi_kategori = detay.find_all('div', class_='_1bVOdb')
+        if len(bilgi_kategori) == 2:
+            kategori = bilgi_kategori[0].text.strip()
+            bilgi = bilgi_kategori[1].text.strip()
+            print(f"{kategori}: {bilgi}")
+
+    print('------------------------------')
